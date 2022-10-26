7850b9b5c9f9caac5904f1496c0687af8dc86eb2
Jose Antonio <antonio.gutierrezc@hotmail.com>
2022-10-26 08:55:03 -0500

    se agrego boton regresar

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const fetch = require("isomorphic-fetch");
 
 const listOfDrinks = document.getElementById('busqueda');
 const loadMoreBtn = document.getElementById('enviar');
+const regresar = document.getElementById('botonMostrar');
+regresar.style.display = "none"
 
 const loadMargaritaInstruction = (userQuery) => {
   const DRINKS_URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + userQuery;
@@ -27,6 +29,26 @@ const loadMargaritaInstruction = (userQuery) => {
         drinkType.innerText = drink.strDrink;
         drinkType.classList.add("drink-type")
         listOfDrinks.append(drinkType);
+
+        drinkType.addEventListener("click", () => {
+          console.log(drinkType.innerText)
+          document.querySelector(".titulo").style.display = "none"
+          document.querySelector(".flex-container").style.display = "none"
+          document.querySelector(".busqueda").style.display = "none"
+          regresar.style.display = "block"
+
+          console.log(drink, "---")
+
+
+
+        })
+        regresar.addEventListener("click", () => {
+          document.querySelector(".titulo").style.display = "block"
+          document.querySelector(".flex-container").style.display = "flex"
+          document.querySelector(".busqueda").style.display = "block"
+          regresar.style.display = "none"
+          
+        })
       }
       console.log(responseJson)
     })
@@ -35,6 +57,7 @@ const loadMargaritaInstruction = (userQuery) => {
 const inputQuery = document.getElementById("user-input")
 loadMoreBtn.addEventListener('click', () => {
   loadMargaritaInstruction(inputQuery.value);
+
 
 })
 
