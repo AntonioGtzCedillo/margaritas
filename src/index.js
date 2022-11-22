1b4836d46cf4ee1cec9f82570135ab569290a788
Jose Antonio <antonio.gutierrezc@hotmail.com>
2022-11-22 16:44:46 -0600

    modificacion de bacground theme y bordes

--- src/index.js
+++ src/index.js
@@ -14,21 +14,44 @@ instructionsDiv.style.display = "none"
 ingredientsDiv.style.display = "none"
 flexContainer.style.display = "none"
 
-document.querySelector(".cbox1").addEventListener('change', (event) => {
-  if (event.target.checked) {
-    document.querySelector(".flex-container").style.border = "2px solid rgb(0, 34, 255)";
-  } else {
-    document.querySelector(".flex-container").style.border = "none";
-  }
-})
-document.querySelector(".cbox1").addEventListener('change', (event) => {
-  if (event.target.checked) {
-    document.getElementById("user-input").style.border = "4px solid rgb(0, 255, 204)";
-  } else {
-    document.getElementById("user-input").style.border = "none";
-  }
-})
 
+const addBorders = () => {
+  document.querySelector(".flex-container").style.border = "2px solid rgb(0, 34, 255)"
+  document.getElementById("infoBebidaDiv").style.border = "4px solid greenyellow"
+  document.querySelector(".flex-container-ddp").style.border = "4px solid rgb(255, 34, 0)"
+  document.getElementById("user-input").style.border = "4px solid rgb(0, 255, 204)"
+
+
+}
+const removeBorders = () => {
+  document.querySelector(".flex-container").style.border = "none"
+  document.getElementById("infoBebidaDiv").style.border = "none"
+  document.querySelector(".flex-container-ddp").style.border = "none"
+  document.getElementById("user-input").style.border = "none"
+
+}
+const activateDarkTheme = () => {
+  document.querySelector("h1").classList.add("darkLayout")
+  document.querySelector("body").classList.add("darkLayout")
+
+
+}
+const disableDarkTheme = () => {
+  document.querySelector("h1").classList.remove("darkLayout")
+  document.querySelector("body").classList.remove("darkLayout")
+
+}
+
+document.querySelector(".cbox1").addEventListener('change', (event) => {
+  if (event.target.checked) {
+    removeBorders()
+    activateDarkTheme()
+  } else {
+    addBorders()
+    disableDarkTheme()
+
+  }
+})
 
 const loadMargaritaInstruction = (userQuery) => {
   const DRINKS_URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + userQuery;
@@ -101,29 +124,21 @@ const loadMargaritaInstruction = (userQuery) => {
               borrarIngredientes[index].remove()
 
             }
-          }          
-          document.querySelector(".cbox1").addEventListener('change', (event) => {
-            if (event.target.checked) {
-              document.getElementById("infoBebidaDiv").style.border = "4px solid greenyellow";
-            } else {
-              document.getElementById("infoBebidaDiv").style.border = "none";
-            }
-          })
-          document.querySelector(".cbox1").addEventListener('change', (event) => {
-            if (event.target.checked) {
-              document.querySelector(".flex-container-ddp").style.border = "4px solid rgb(255, 34, 0)";
-            } else {
-              document.querySelector(".flex-container-ddp").style.border = "none";
-            }
-          })
-          document.querySelector(".cbox1").addEventListener('change', (event) => {
-            if (event.target.checked) {
-              document.querySelector(".flex-container").style.border = "2px solid rgb(0, 34, 255)";
-            } else {
-              document.querySelector(".flex-container").style.border = "none";
-            }
-          })
-          
+          }
+          // document.querySelector(".cbox1").addEventListener('change', (event) => {
+          //   if (event.target.checked) {
+          //     document.getElementById("infoBebidaDiv").style.border = "4px solid greenyellow";
+          //     document.querySelector(".flex-container-ddp").style.border = "4px solid rgb(255, 34, 0)";
+          //     document.querySelector(".flex-container").style.border = "2px solid rgb(0, 34, 255)";
+          //     document.getElementById("user-input").style.border = "4px solid rgb(0, 255, 204)";
+          //   } else {
+          //     document.getElementById("infoBebidaDiv").style.border = "none";
+          //     document.querySelector(".flex-container-ddp").style.border = "none";
+          //     document.querySelector(".flex-container").style.border = "none";
+          //   }
+          // })
+
+
 
           //mostar informacion de la bebida seleccionada (instrcciones, imagen = ddp (drink detail page)) 
           const infomacionBebida = document.createElement("p")
@@ -152,9 +167,9 @@ const loadMargaritaInstruction = (userQuery) => {
             strIngredient13: drink.strIngredient13,
             strIngredient14: drink.strIngredient14,
             strIngredient15: drink.strIngredient15,
-            
+
           }
-//mostrar ingredientes usando operador ternario
+          //mostrar ingredientes usando operador ternario
           const parrafoIngredientes = `${ingredientes.strIngredient1 !== null ? ingredientes.strIngredient1 + (ingredientes.strIngredient2 !== null ? ", " : ".") : ""}${ingredientes.strIngredient2 !== null ? ingredientes.strIngredient2 + (ingredientes.strIngredient3 !== null ? ", " : ".") : ""}${ingredientes.strIngredient3 !== null ? ingredientes.strIngredient3 + (ingredientes.strIngredient4 !== null ? ", " : ".") : ""}${ingredientes.strIngredient4 !== null ? ingredientes.strIngredient4 + (ingredientes.strIngredient5 !== null ? ", " : ".") : ""}${ingredientes.strIngredient5 !== null ? ingredientes.strIngredient5 + (ingredientes.strIngredient6 !== null ? ", " : ".") : ""}${ingredientes.strIngredient6 !== null ? ingredientes.strIngredient6 + (ingredientes.strIngredient7 !== null ? ", " : ".") : ""}`
 
           console.log(parrafoIngredientes)
@@ -184,7 +199,7 @@ const loadMargaritaInstruction = (userQuery) => {
 
           for (let index = oldParagraphs.length - 1; index >= 0; index--) {
             oldParagraphs[index].remove()
-  
+
           }
 
 
